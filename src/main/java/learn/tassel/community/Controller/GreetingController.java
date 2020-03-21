183e62d667f82b140682bfec4a749c82268f348d
Mytassel <ningzhiang@126.com>
2020-03-21 15:20:29 +0800

    add index page and bootstrap

--- src/main/java/learn/tassel/community/Controller/GreetingController.java
+++ src/main/java/learn/tassel/community/Controller/GreetingController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    //初始路径
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
