6792bf2abe8715c4b9708d7ae60fd9d6e47447b4
Mytassel <ningzhiang@126.com>
2020-03-22 21:44:56 +0800

    add session to login

--- src/main/java/learn/tassel/community/Controller/AuthorizeController.java
+++ src/main/java/learn/tassel/community/Controller/AuthorizeController.java
@@ -4,27 +4,41 @@ import learn.tassel.community.Dto.AccessTokenDTO;
 import learn.tassel.community.Dto.GitHubUser;
 import learn.tassel.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 //Controller 路由Api的承载者
 public class AuthorizeController {
 
+    //装配类
     @Autowired
     private GitHubProvider gitHubProvider;
 
+    //装配配置文件内容值
+    @Value("${github.client.id}")
+    private String ClientID;
+
+    @Value("${github.client.secret}")
+    private String ClientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String RedirectUri;
+
 //    用于接受github账号登陆验证
     @GetMapping("/callback")
-    public String callBack(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state){
+    public String callBack(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request){
 
         //引入jar 或依赖 alt+回车
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("1ceec699e187a3eed6fa");
-        accessTokenDTO.setClient_secret("e2ce8560494a08d15c6d92a08cdd7672be31c960");
+        accessTokenDTO.setClient_id(ClientID);
+        accessTokenDTO.setClient_secret(ClientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:9090/callback");
+        accessTokenDTO.setRedirect_uri(RedirectUri);
         accessTokenDTO.setState(state);
         //得到方法返回值 ctrl+ alt + v
         String accessToken = gitHubProvider.getAccessToken(accessTokenDTO);
@@ -34,7 +48,15 @@ public class AuthorizeController {
         //获取token; access_token
         String access_token = accessToken.split("&")[0].split("=")[1];
         GitHubUser user = gitHubProvider.getGitHubUser(access_token);
-        System.out.println(user.getName());
-        return "index";
+
+        //手写session
+        if(user != null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else{
+            return "redirect:/";
+        }
+
+
     }
 }
